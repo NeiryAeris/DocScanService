8fdd7305c6816fb511110f80ac7a2a8ba6d6b914
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-12-22 05:31:16 +0700

    Update deps.py

--- processing_python/app/core/deps.py
+++ processing_python/app/core/deps.py
@@ -1,7 +1,8 @@
 from fastapi import Header, HTTPException, status
 from .config import INTERNAL_TOKEN
 
-async def verify_internal_token(x_internal_token: str = Header(...)):
+async def verify_internal_token(
+    x_internal_token: str = Header(..., alias="X-Internal-Token")):
     if x_internal_token != INTERNAL_TOKEN:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,

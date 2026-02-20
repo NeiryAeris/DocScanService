31159b394b47434deca407540bbe1b6ec272b9a6
NeiryAeris <Kukorysenpai3506@gmail.com>
2026-02-20 09:49:57 +0700

    Update bootstrap.ts

--- backend_node/apps/gateway/src/bootstrap.ts
+++ backend_node/apps/gateway/src/bootstrap.ts
@@ -5,26 +5,6 @@ import type { Express, RequestHandler } from "express";
 
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter'
 
-// function loadLegacyMiddleware(): RequestHandler {
-//   // Support multiple export styles from ./app.ts
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const mod = require("./app");
-
-//   const candidate = mod?.default ?? mod?.app ?? mod;
-
-//   if (typeof candidate !== "function") {
-//     const keys =
-//       mod && typeof mod === "object" && !Array.isArray(mod) ? Object.keys(mod) : [];
-//     throw new Error(
-//       `Legacy express app export is not a middleware function. ` +
-//         `Expected src/app.ts to export: default OR named export { app }. ` +
-//         `Got type="${typeof candidate}". Module keys=[${keys.join(", ")}]`
-//     );
-//   }
-
-//   return candidate as RequestHandler;
-// }
-
 import legacyApp from './app'
 
 export async function configureApp(app: INestApplication) {
